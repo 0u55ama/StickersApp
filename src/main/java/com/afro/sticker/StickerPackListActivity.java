eb6562fa3c45466c8394e90fe191b304b56f4f98
Oussama <oussamaait2001@gmail.com>
2022-02-17 17:45:46 +0000

    ccc

---
+++ src/main/java/com/afro/sticker/StickerPackListActivity.java
@@ -0,0 +1,263 @@
+/*
+ * Copyright (c) WhatsApp Inc. and its affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.afro.sticker;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
+
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class StickerPackListActivity extends AddStickerPackActivity {
+    public static final String EXTRA_STICKER_PACK_LIST_DATA = "sticker_pack_list";
+    private static final int STICKER_PREVIEW_DISPLAY_LIMIT = 5;
+    private LinearLayoutManager packLayoutManager;
+    private RecyclerView packRecyclerView;
+    private StickerPackListAdapter allStickerPacksListAdapter;
+    private WhiteListCheckAsyncTask whiteListCheckAsyncTask;
+    private ArrayList<StickerPack> stickerPackList;
+    private AdView mAdView;
+    private InterstitialAd mInterstitialAd;
+    private InterstitialAd interstitialAd;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sticker_pack_list);
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+
+            InterstitialAd.load(this,"ca-app-pub-9487995294051736/4019671910", adRequest,
+                    new InterstitialAdLoadCallback() {
+                        @Override
+                        public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                            // The mInterstitialAd reference will be null until
+                            // an ad is loaded.
+                            mInterstitialAd = interstitialAd;
+                            Log.i("TAG", "onAdLoaded");
+                        }
+
+                        @Override
+                        public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                            // Handle the error
+                            Log.i("TAG", loadAdError.getMessage());
+                            mInterstitialAd = null;
+                        }
+                    });
+
+
+        showintr();
+        showintr();
+        showintr();
+
+
+
+
+
+
+        packRecyclerView = findViewById(R.id.sticker_pack_list);
+        stickerPackList = getIntent().getParcelableArrayListExtra(EXTRA_STICKER_PACK_LIST_DATA);
+        showStickerPackList(stickerPackList);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(getResources().getQuantityString(R.plurals.title_activity_sticker_packs_list, stickerPackList.size()));
+        }
+
+    }
+    private void showintr() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (mInterstitialAd != null) {
+                    mInterstitialAd.show(StickerPackListActivity.this);
+                } else {
+                    Log.d("TAG", "The interstitial ad wasn't ready yet.");
+                }
+            }
+        },4000);
+
+
+    }
+
+    /***
+    public void loadAd() {
+        AdRequest adRequest = new AdRequest.Builder().build();
+        InterstitialAd.load(
+                this,
+                AD_UNIT_ID,
+                adRequest,
+                new InterstitialAdLoadCallback() {
+                    @Override
+                    public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                        // The mInterstitialAd reference will be null until
+                        // an ad is loaded.
+                        StickerPackListActivity.this.interstitialAd = interstitialAd;
+                        Log.i(TAG, "onAdLoaded");
+                        Toast.makeText(StickerPackListActivity.this, "onAdLoaded()", Toast.LENGTH_SHORT).show();
+                        interstitialAd.setFullScreenContentCallback(
+                                new FullScreenContentCallback() {
+                                    @Override
+                                    public void onAdDismissedFullScreenContent() {
+                                        // Called when fullscreen content is dismissed.
+                                        // Make sure to set your reference to null so you don't
+                                        // show it a second time.
+                                        StickerPackListActivity.this.interstitialAd = null;
+                                        Log.d("TAG", "The ad was dismissed.");
+                                    }
+
+                                    @Override
+                                    public void onAdFailedToShowFullScreenContent(AdError adError) {
+                                        // Called when fullscreen content failed to show.
+                                        // Make sure to set your reference to null so you don't
+                                        // show it a second time.
+                                        StickerPackListActivity.this.interstitialAd = null;
+                                        Log.d("TAG", "The ad failed to show.");
+                                    }
+
+                                    @Override
+                                    public void onAdShowedFullScreenContent() {
+                                        // Called when fullscreen content is shown.
+                                        Log.d("TAG", "The ad was shown.");
+                                    }
+                                });
+                    }
+
+                    @Override
+                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                        // Handle the error
+                        Log.i(TAG, loadAdError.getMessage());
+                        interstitialAd = null;
+
+                        String error =
+                                String.format(
+                                        "domain: %s, code: %d, message: %s",
+                                        loadAdError.getDomain(), loadAdError.getCode(), loadAdError.getMessage());
+                        Toast.makeText(
+                                StickerPackListActivity.this, "onAdFailedToLoad() with error: " + error, Toast.LENGTH_SHORT)
+                                .show();
+                    }
+                });
+    }
+
+
+    private void showInterstitial() {
+        // Show the ad if it's ready. Otherwise toast and restart the game.
+        if (mInterstitialAd != null) {
+            mInterstitialAd.show(this);
+        } else {
+            Log.d("TAG", "The interstitial ad wasn't ready yet.");
+        }
+    }
+
+ */
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        whiteListCheckAsyncTask = new WhiteListCheckAsyncTask(this);
+        whiteListCheckAsyncTask.execute(stickerPackList.toArray(new StickerPack[0]));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (whiteListCheckAsyncTask != null && !whiteListCheckAsyncTask.isCancelled()) {
+            whiteListCheckAsyncTask.cancel(true);
+        }
+    }
+
+    private void showStickerPackList(List<StickerPack> stickerPackList) {
+        allStickerPacksListAdapter = new StickerPackListAdapter(stickerPackList, onAddButtonClickedListener);
+        packRecyclerView.setAdapter(allStickerPacksListAdapter);
+        packLayoutManager = new LinearLayoutManager(this);
+        packLayoutManager.setOrientation(RecyclerView.VERTICAL);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(
+                packRecyclerView.getContext(),
+                packLayoutManager.getOrientation()
+        );
+        packRecyclerView.addItemDecoration(dividerItemDecoration);
+        packRecyclerView.setLayoutManager(packLayoutManager);
+        packRecyclerView.getViewTreeObserver().addOnGlobalLayoutListener(this::recalculateColumnCount);
+    }
+
+
+    private final StickerPackListAdapter.OnAddButtonClickedListener onAddButtonClickedListener = pack ->  {
+        addStickerPackToWhatsApp(pack.identifier, pack.name);
+
+    };
+
+    private void recalculateColumnCount() {
+        final int previewSize = getResources().getDimensionPixelSize(R.dimen.sticker_pack_list_item_preview_image_size);
+        int firstVisibleItemPosition = packLayoutManager.findFirstVisibleItemPosition();
+        StickerPackListItemViewHolder viewHolder = (StickerPackListItemViewHolder) packRecyclerView.findViewHolderForAdapterPosition(firstVisibleItemPosition);
+        if (viewHolder != null) {
+            final int widthOfImageRow = viewHolder.imageRowView.getMeasuredWidth();
+            final int max = Math.max(widthOfImageRow / previewSize, 1);
+            int maxNumberOfImagesInARow = Math.min(STICKER_PREVIEW_DISPLAY_LIMIT, max);
+            int minMarginBetweenImages = (widthOfImageRow - maxNumberOfImagesInARow * previewSize) / (maxNumberOfImagesInARow - 1);
+            allStickerPacksListAdapter.setImageRowSpec(maxNumberOfImagesInARow, minMarginBetweenImages);
+        }
+    }
+
+
+    static class WhiteListCheckAsyncTask extends AsyncTask<StickerPack, Void, List<StickerPack>> {
+        private final WeakReference<StickerPackListActivity> stickerPackListActivityWeakReference;
+
+        WhiteListCheckAsyncTask(StickerPackListActivity stickerPackListActivity) {
+            this.stickerPackListActivityWeakReference = new WeakReference<>(stickerPackListActivity);
+        }
+
+        @Override
+        protected final List<StickerPack> doInBackground(StickerPack... stickerPackArray) {
+            final StickerPackListActivity stickerPackListActivity = stickerPackListActivityWeakReference.get();
+            if (stickerPackListActivity == null) {
+                return Arrays.asList(stickerPackArray);
+            }
+            for (StickerPack stickerPack : stickerPackArray) {
+                stickerPack.setIsWhitelisted(WhitelistCheck.isWhitelisted(stickerPackListActivity, stickerPack.identifier));
+            }
+            return Arrays.asList(stickerPackArray);
+        }
+
+        @Override
+        protected void onPostExecute(List<StickerPack> stickerPackList) {
+            final StickerPackListActivity stickerPackListActivity = stickerPackListActivityWeakReference.get();
+            if (stickerPackListActivity != null) {
+                stickerPackListActivity.allStickerPacksListAdapter.setStickerPackList(stickerPackList);
+                stickerPackListActivity.allStickerPacksListAdapter.notifyDataSetChanged();
+            }
+        }
+    }
+}
